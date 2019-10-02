2d235c1eee0c24104bc0961b10dd25cc9f2e570d
dilip\ jat <dilipjat98@gmail.com>
2019-10-02 23:37:06 +0530

    profile comfirmation done testing left

--- authentication/urls.py
+++ authentication/urls.py
@@ -4,4 +4,5 @@ from . import views
 app_name='authentication'
 urlpatterns = [
     path('login/',views.logIn, name='login'),
+    path('profile/',views.profileComfirm, name='profile_comfirm'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -1,7 +1,8 @@
-from django.http import HttpResponse
-from django.shortcuts import redirect
+from django.http import HttpResponse,Http404
+from django.shortcuts import redirect,render
 import requests as re
 from crawlr_web import settings
+import json
 
 def logIn(request):
      return HttpResponse('<a href="https://crawlr-api.herokuapp.com/auth/linkedin">login</a>')
@@ -20,7 +21,28 @@ def linkedInTokenHandle(request):
             responce = re.post(url,headers = headers)
             return HttpResponse(responce.status_code)
         else:
-            return HttpResponse(jwt_token)
-    else:
-        return redirect('/auth/login')
-    return HttpResponse('kk')
+            return render(request,'profile_comfirmation.html',jwt_token)
+    return redirect('/auth/login')
+    # jwt_token = {'provider':'sfs','id':'1','fullName':'dilip','image':'as'}
+    # return render(request,'profile_comfirmation.html',jwt_token)
+
+def profileComfirm(request):
+    if request.method == 'POST':
+        provider = request.POST.get('provider')
+        id = request.POST.get('id')
+        fullName = request.POST.get('fullName')
+        image = request.POST.get('image')
+        data = {'provider':provider,'id':id,'fullName':fullName,'image':image}
+        headers = {'content-type': 'application/json'}
+        responce = re.post(settings.API_URL+'/auth/comfirm',data=json.dumps(data),headers=headers)
+        if responce.status_code == re.codes.ok:
+            jwt_token = responce.json()
+            if 'JWT' in jwt_token.keys():
+                token = jwt_token['JWT']
+                headers = {'content-type': 'application/json','authorization':token}
+                url = settings.API_URL+'/auth/test'
+                responce = re.post(url,headers = headers)
+                return HttpResponse(responce.status_code)
+            else:
+                return Http404('something went wrong')
+        return redirect('auth/login')
