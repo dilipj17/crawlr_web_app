d5ca264c7abe581cada2c690d5cb6690fb5d2390
dilip jat <dilip.j17@iiits.in>
2019-11-12 16:53:51 +0530

    Add static folder and infinite js

--- crawlr_web/urls.py
+++ crawlr_web/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path,include,re_path
 from authentication.views import linkedInTokenHandle
+from . import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('auth/',include('authentication.urls',namespace='auth')),
     path('ques/',include('question.urls',namespace='ques')),
     re_path('login/',linkedInTokenHandle)
 ]
+if settings.DEBUG:
+    urlpatterns = urlpatterns +  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns = urlpatterns +  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- question/views.py
+++ question/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from crawlr_web.decorators import login_required
 from django.http import HttpResponse
+import requests as re
 
 @login_required
 def QuestionList(request):
