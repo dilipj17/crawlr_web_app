153afc4489abfd4a677945284381272fd67497c4
dilip jat <dilip.j17@iiits.in>
2019-12-11 01:07:07 +0530

    profile and reply option added

--- static/js/profile.js
+++ static/js/profile.js
@@ -10,8 +10,23 @@ $(document).ready(function() {
 
       responce = JSON.parse(res);
       if (responce.status == 200) {
-        // write responce to give location
-        $("#profile-data").html(responce.fullName);
+        $("#profileimage1").html(
+          '<img  class="rounded-circle z-depth-2" src="' +
+            responce.image +
+            '" width="32" height="32"></img>'
+        );
+        $("#profileimage2").html(
+          '<img  class="rounded-circle z-depth-2" src="' +
+            responce.image +
+            '" width="80" height="80"></img>'
+        );
+        $("#fullname1").html(responce.fullName);
+        $("#fullname2").html(responce.fullName);
+        $("#email").html(responce.email);
+        $("#about").html(responce.bio);
+        $("#searches").html(responce.searches);
+        $("#questions").html(responce.questions);
+        $("#replys").html(responce.karma);
       } else {
         console.log("some error occured");
       }
