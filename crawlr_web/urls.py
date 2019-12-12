a08a673dbc34815dd7278c6f4f4d8dbfcd8093a8
dilip jat <dilip.j17@iiits.in>
2019-12-13 01:17:16 +0530

    experiment

--- crawlr_web/urls.py
+++ crawlr_web/urls.py
@@ -13,3 +13,7 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns = urlpatterns +  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns = urlpatterns +  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
